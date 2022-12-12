73afb655715119cdfb6fb86f4b2f39326a953516
Andrii Lysenko <s0fcat@gmail.com>
2022-12-12 18:53:03 +0100

    fixed NotFound redirecting

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,23 +1,25 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-    setTimeout(() => {
-      navigate('/')
-    }, 2000)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    const timeout = setTimeout(() => {
+      navigate("/");
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  });
 
   return (
     <>
       <div>Page not found</div>
       <div>Redirecting...</div>
     </>
-  )
-}
+  );
+};
 
-export default NotFound
-
+export default NotFound;
