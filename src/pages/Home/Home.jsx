051b52446d33be0688b15eeef4f78319cce4ae9b
Akril0 <alexbatin2003@gmail.com>
2023-01-23 16:45:53 +0200

    Change scrols

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import styles from './Home.module.scss';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>item1</div>
+      <div className={styles.item}>item2</div>
+      <div className={styles.item}>item3</div>
+      <div className={styles.item}>item4</div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
